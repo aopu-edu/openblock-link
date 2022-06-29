b20e74483417bedf78eb5bebc416ec73b14c2f6a
liucy427 <liucy427@163.com>
2022-06-29 18:09:47 +0800

    Modify library file commit logic: (1) The uploaded library files will no longer be uploaded (2) If the subspace of the board is insufficient, upload the firmware again

--- src/upload/microPython.js
+++ src/upload/microPython.js
@@ -19,7 +19,7 @@ class MicroPython {
         this._sendstd = sendstd;
 
         if (os.platform() === 'darwin') {
-            this._pyPath = path.join(this._pythonPath, 'bin/python');
+            this._pyPath = path.join(this._pythonPath, 'python3');
         } else {
             this._pyPath = path.join(this._pythonPath, 'python');
         }
@@ -31,6 +31,9 @@ class MicroPython {
         }
 
         this._codefilePath = path.join(this._projectPath, 'main.py');
+        this._existedLibFile = []; // Existing files in the board
+        this._bfree = -1; // block free of board
+        this._bsize = 0; // block size of board
     }
 
     async flash (code, library = []) {
@@ -59,7 +62,23 @@ class MicroPython {
 
         // If we can not entry raw REPL, we should flash micro python firmware first.
         try {
-            await this.checkReplSupport();
+            await this.checkLibFileList();
+            await this.checkRestSpace();
+
+            const reflashFirmware = this.judgeReflashFirmware(fileToPut, this._config.chip);
+            if (reflashFirmware) {
+                // If the subspace of the board is insufficient, uploaded the firmware again
+                this._sendstd(`${ansi.yellow_dark}The space of board is insufficient.\n`);
+                this._sendstd(`${ansi.clear}Try to flash micropython firmware to ` +
+                    `refresh the memory space of the board.\n`);
+                
+                try {
+                    this._existedLibFile = [];
+                    await this.flashFirmware();
+                } catch (e) {
+                    return Promise.reject(e);
+                }
+            }
         } catch (err) {
             this._sendstd(`${ansi.yellow_dark}Could not enter raw REPL.\n`);
             this._sendstd(`${ansi.clear}Try to flash micro python firmware to fix.\n`);
@@ -74,18 +93,92 @@ class MicroPython {
         this._sendstd('Writing files...\n');
 
         for (const file of fileToPut) {
-            try {
-                await this.obmpyPut(file);
-            } catch (err) {
-                return Promise.reject(err);
+            const fileName = file.substring(file.lastIndexOf('\\') + 1);
+            const pushed = this._existedLibFile.find(item => fileName === item);
+            if (!pushed || fileName === 'main.py') {
+                try {
+                    await this.obmpyPut(file);
+                } catch (err) {
+                    return Promise.reject(err);
+                }
+            } else {
+                this._sendstd(`${file} already writed\n`);
             }
+            
         }
 
         this._sendstd(`${ansi.green_dark}Success\n`);
         return Promise.resolve();
     }
 
-    checkReplSupport () {
+    // Check whether the subspace on the board is sufficient
+    // to determine whether to upload the firmware again
+    judgeReflashFirmware (fileToPut, boardType) {
+        let totalSize = 0;
+        fileToPut.forEach(file => {
+            const fileName = file.substring(file.lastIndexOf('\\') + 1);
+            const exsisted = this._existedLibFile.find(item => fileName === item);
+            if (!exsisted || fileName === 'main.py') {
+                const fileSize = fs.statSync(file).size;
+                if (boardType === 'k210') {
+                    // In k210, the files are densely packed
+                    totalSize += fileSize;
+                } else {
+                    // In esp32 && esp8266, the files are stored by block
+                    totalSize += Math.ceil(fileSize / this._bsize);
+                }
+            }
+        });
+        let reflashFirmware = 0;
+        if (boardType === 'k210') {
+            // When space of k210 is less than 100 bytes, reflash firmware
+            reflashFirmware = ((this._bfree * this._bsize) - totalSize < 100) ? 1 : 0;
+        } else {
+            // When space of esp32 or esp8266 is less than 2 blocks, reflash firmware
+            reflashFirmware = (this._bfree - totalSize) < 2 ? 1 : 0;
+        }
+        
+        return reflashFirmware;
+    }
+
+    checkRestSpace () {
+        this._sendstd(`Try to check rest space.\n`);
+
+        return new Promise((resolve, reject) => {
+            const arg = [
+                `-m${OBMPY_MODULE_NAME}`,
+                `-p${this._peripheralPath}`,
+                '-d1', // delay 1s to wait for device ready
+                `-r${this._config.rtsdtr === false ? 'F' : 'T'}`,
+                'restspace'
+            ];
+
+            if (this._config.chip === 'k210') {
+                arg.splice(4, 0, '-a1'); // if k210 just send abort command once
+                arg.splice(6, 0, '/flash');
+            }
+
+            const obmpy = spawn(this._pyPath, arg);
+            
+            obmpy.stdout.on('data', buf => {
+                // It seems that avrdude didn't use stdout.
+                const data = JSON.parse(buf.toString().trim()
+                    .replace(new RegExp('\'', 'g'), '"'));
+                this._bsize = data.bsize;
+                this._bfree = data.bfree;
+            });
+            obmpy.on('exit', outCode => {
+                switch (outCode) {
+                case 0:
+                    return resolve();
+                default:
+                    return reject();
+                }
+            });
+        });
+    }
+
+    checkLibFileList () {
         this._sendstd(`Try to enter raw REPL.\n`);
 
         return new Promise((resolve, reject) => {
@@ -102,6 +195,13 @@ class MicroPython {
             }
 
             const obmpy = spawn(this._pyPath, arg);
+
+            obmpy.stdout.on('data', buf => {
+                // It seems that avrdude didn't use stdout.
+                let data = buf.toString().trim();
+                data = data.replace(new RegExp('[/\\r]', 'g'), '');
+                this._existedLibFile = data.split('\n');
+            });
 
             obmpy.on('exit', outCode => {
                 switch (outCode) {
